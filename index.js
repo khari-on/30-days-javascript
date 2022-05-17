3e7ebf793d661203f76680475ea74806254564f3
Hari <Khariprasath351@gmail.com>
2022-05-17 19:49:31 +0530

    day-1 in javascript

---
+++ index.js
@@ -0,0 +1,71 @@
+
+/*
+Write a single line comment which says, comments can make code readable
+
+Write another single comment which says, Welcome to 30DaysOfJavaScript
+
+Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+Declare four variables without assigning values
+
+Declare four variables with assigned values
+
+Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+Declare variables to store your first name, last name, marital status, country and age in a single line
+
+Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+*/
+
+
+
+
+
+//ANSWER SECTION
+
+//comments can make code readable
+
+//Welcome to 30DaysOfJavaScript
+
+/*
+comments can make code readable, easy to reuse and informative
+*/
+
+//6)
+// let cat;
+// let dog;
+// let horse;
+// let fox;
+
+//7)
+// let flower ="rose";
+// let animal ="tiger";
+// let country ="india";
+// let sports="kabadi";
+
+
+//8)
+
+// let firstName ="Hari";
+// let lastName="Prasath";
+// let martialStatus="single";
+// let country="India";
+// let age=21;
+
+
+//9)
+
+//let firstName ="Hari",lastName="Prasath", martialStatus="single", country="India", age=21;
+
+
+//10)
+/*
+let myAge=21;
+let yourAge = 33;
+
+console.log(myAge,yourAge)
+*/
---
+++ varible.js
@@ -0,0 +1,13 @@
+let x = "hello"   //string
+
+let y= true;      //boolean
+
+let c;            //undefined
+
+let d=null;       //null datatypes 
+
+
+console.log(typeof(x))
+console.log(typeof(y))
+console.log(typeof(c))
+console.log(typeof(d))
